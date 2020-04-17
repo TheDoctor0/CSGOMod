b9732ffbdb0160dd186a43d96f015cb6c34824e3
The Doctor <dawid.janik95@gmail.com>
2020-04-17 19:17:49 +0200

    Rework generation script logic - separate for default model.

--- skins/ak47/generate.py
+++ skins/ak47/generate.py
@@ -12,20 +12,23 @@ compiler = "studiomdl.exe"
 weapon = os.path.basename(os.path.normpath(os.getcwd()))
 fresh = len(sys.argv) > 1
 
-if not os.path.isfile("{}.smd".format(weapon)):
-    print("Template file {}.smd does not exists!".format(weapon))
+if not os.path.isfile("template.smd"):
+    print("Template SMD file (template.smd) does not exists!")
     exit(0)
 
-if not os.path.isfile("v_{}.qc".format(weapon)):
-    print("Template file v_{}.qc does not exists!".format(weapon))
+if not os.path.isfile("template.qc"):
+    print("Template QC file (template.qc) does not exists!")
     exit(0)
 
-if not os.path.isfile("v_{}.qc".format(weapon)):
-    print("Default weapon skin {}_default.bmp does not exists!".format(weapon))
+if not os.path.isfile("default.bmp"):
+    print("Default weapon texture file (default.bmp) does not exists!")
     exit(0)
 
-skins = glob.glob("{}_[!default]*.bmp".format(weapon))
-skins = ["{}_default.bmp".format(weapon)] + skins
+if not os.path.isfile("default.qc"):
+    print("Default weapon QC file (default.qc) does not exists!")
+    exit(0)
+
+skins = glob.glob("{}_*.bmp".format(weapon))
 
 if fresh:
     files = glob.glob("v_{}_*.qc".format(weapon))
@@ -37,7 +40,7 @@ for index, skin in enumerate(skins, start=0):
     smd = skin.replace(".bmp", ".smd")
 
     if fresh or not os.path.isfile(smd):
-        shutil.copyfile("{}.smd".format(weapon), smd)
+        shutil.copyfile("template.smd", smd)
 
         with fileinput.FileInput(smd, inplace=True) as file:
             for line in file:
@@ -47,7 +50,7 @@ for index, skin in enumerate(skins, start=0):
     filename = "v_{}_{}.qc".format(weapon, order)
 
     if not os.path.isfile(filename):
-        shutil.copyfile("v_{}.qc".format(weapon), filename)
+        shutil.copyfile("template.qc", filename)
 
         with fileinput.FileInput(filename, inplace=True) as file:
             for line in file:
@@ -68,6 +71,7 @@ for index, skin in enumerate(skins, start=0):
                 print(line, end='')
 
 files = glob.glob("v_{}_*.qc".format(weapon))
+files = ["default.qc"] + files
 
 for file in files:
     os.system("{} {}".format(compiler, file))
